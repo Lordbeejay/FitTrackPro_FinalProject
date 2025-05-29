a9c2dbbf0d72bfbedfe1ce36d16dec64d810e0ed
Brethren Ace D. de la Gente <bddelagente@up.edu.ph>
2025-05-30 05:12:40 +0800

    [fix] Device Testing
    Added lines to allow application to work on WIndows by disabling Firebase.

--- fitrack_pro/lib/main.dart
+++ fitrack_pro/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fitrack_pro/core/services/auth_service.dart';
@@ -12,11 +13,18 @@ import 'package:fitrack_pro/features/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  if (defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.iOS ||
+      defaultTargetPlatform == TargetPlatform.macOS) {
+    await Firebase.initializeApp();
+  }
+
   await NotificationHelper.initialize();
 
   runApp(const WorkoutApp());
 }
+
 
 class WorkoutApp extends StatefulWidget {
   const WorkoutApp({super.key});
